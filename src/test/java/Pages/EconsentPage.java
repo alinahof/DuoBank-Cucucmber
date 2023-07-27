8579ee9028144df6058d5d374d41787779793abe
Alina Hoffman <alinahoffman1@gmail.com>
2023-07-27 14:57:56 -0400

    updated

--- src/test/java/Pages/EconsentPage.java
+++ src/test/java/Pages/EconsentPage.java
@@ -1,6 +1,33 @@
 package Pages;
 
+import lombok.Data;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utils.Driver;
+
+@Data
 public class EconsentPage {
 
+    public EconsentPage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
 
+    @FindBy(id = "eConsentdeclarerFirstName")
+    private WebElement firstName;
+
+    @FindBy(id = "eConsentdeclarerLastName")
+    private WebElement lastName;
+
+    @FindBy(id = "eConsentdeclarerEmail")
+    private WebElement email;
+
+    @FindBy(id = "agree")
+    private WebElement agreeButton;
+
+    @FindBy(id = "dontagree")
+    private WebElement dontagreeButton;
+
+    @FindBy(xpath = " //a[@href='#next']")
+    private WebElement nextButton;
 }
