aa7ccc2110679733a076af0785c3ad4d2e3199fa
Dr.Gonzo <Gonzo@yahoo.com>
2023-07-28 13:26:09 -0700

    new user story 10 tasks

--- src/test/java/stepDefinitions/SummaryStepDefs.java
+++ src/test/java/stepDefinitions/SummaryStepDefs.java
@@ -26,4 +26,34 @@ public class SummaryStepDefs {
      WebElement successmessage = Driver.getDriver().findElement(By.xpath("//div[@class='alert alert-succes']"));
         Assert.assertTrue("Success message is not displayed.", successmessage.isDisplayed());
     }
+
+    @Given("I click on the personal")
+    public void iClickOnThePersonal() {
+        sum.clickPersonalInfoEditButton();
+        sum.clickSummaryTabButton();
+    }
+
+    @Given("I click on the expense")
+    public void iClickOnTheExpense() {
+        sum.clickExpenseEditButton();
+        sum.clickSummaryTabButton();
+    }
+
+    @Given("I click on the Employment and Income")
+    public void iClickOnTheEmploymentAndIncome() {
+        sum.clickEmploymentAndIncomeEditButton();
+        sum.clickSummaryTabButton();
+    }
+
+    @Given("I click on the Credit Report")
+    public void iClickOnTheCreditReport() {
+        sum.clickOrderCreditEditButton();
+        sum.clickSummaryTabButton();
+    }
+
+    @Given("I click on the eConsent")
+    public void iClickOnTheEConsent() {
+        sum.clickeConsentEditButton();
+        sum.clickSummaryTabButton();
+    }
 }
